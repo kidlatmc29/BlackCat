5d0de5bfcfa71b7a6403f1d8cab4e7592d6d7660
ozabell <kidlatmc@gmail.com>
2020-07-18 16:01:02 -0700

    workingon card.h and card.cpp

--- BlackCat/Card.h
+++ BlackCat/Card.h
@@ -10,8 +10,24 @@
 using namespace std;
 
 class Card {
+    public:
+        enum rank {ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
+            JACK, QUEEN, KING};
+        enum suit {CLUBS, SPADES, DIAMONDS, HEARTS};
 
+        // need to overload << operator to print Card obj to standard output
 
+        Card(rank r = ACE, suit s = SPADES, bool ifu = true); //idk whY this constructor is public??????
 
+        // returns the value of a card, 1 - 11
+        int getValue() const;
+
+        void flip();
+    
+    private:
+        const int HIGHEST_RANK = 10;
+        rank m_Rank;
+        suit m_Suit;
+        bool m_IsFaceUp;
 };
 
