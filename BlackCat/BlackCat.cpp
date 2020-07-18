6b0d674b34a907b7d9c00cab59329c90570b042f
ozabell <kidlatmc@gmail.com>
2020-07-18 15:37:39 -0700

    adding card class

--- BlackCat/BlackCat.cpp
+++ BlackCat/BlackCat.cpp
@@ -1,5 +1,4 @@
-// BlackCat.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// BlackCat.cpp : This file contains the 'main' function. Program execution begins and ends there. This is where the game is played
 
 #include <iostream>
 
---
+++ BlackCat/Card.h
@@ -0,0 +1,17 @@
+// Card.h 
+// A Blackcat playing card for 1 -7 players
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+
+class Card {
+
+
+
+};
+
